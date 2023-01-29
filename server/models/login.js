d7822de3a5808c300a2b01b70e1bf59ea6e2b729
step11angelo <step11angelo@gmail.com>
2023-01-29 14:06:08 +0800

    added login model and migration

---
+++ server/models/login.js
@@ -0,0 +1,30 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class login extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  login.init({
+    username: {
+      type: DataTypes.STRING,
+      allowNul: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNul: false,
+    }
+  }, {
+    sequelize,
+    modelName: 'login',
+  });
+  return login;
+};
