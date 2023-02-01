f2576c7883ed6450a37da59b3bc6ae49c8c0a399
JM Baldonado <john.baldonado@xurpas.com>
2023-02-01 20:51:56 +0800

    give boilerplate to group1

--- server/migrations/20230129053006-create-admin.js
+++ server/migrations/20230129053006-create-admin.js
@@ -1,33 +1,33 @@
-'use strict';
+"use strict";
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('logins', {
+    await queryInterface.createTable("admin", {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       username: {
         type: Sequelize.STRING,
-        allowNull: false
+        allowNull: false,
       },
       password: {
         type: Sequelize.STRING,
-        allowNull: false
+        allowNull: false,
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('logins');
-  }
+    await queryInterface.dropTable("admin");
+  },
 };
